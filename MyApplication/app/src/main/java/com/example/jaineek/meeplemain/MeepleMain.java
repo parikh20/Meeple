0ee6e25a3795fbe0db9d9afd1c75649979f9b510
Jaineek Parikh <parikh20@purdue.edu>
2016-06-07 18:27:34 -0700

    First page switches to second page with a button

--- MyApplication/app/src/main/java/com/example/jaineek/meeplemain/MeepleMain.java
+++ MyApplication/app/src/main/java/com/example/jaineek/meeplemain/MeepleMain.java
@@ -1,20 +1,39 @@
 package com.example.jaineek.meeplemain;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 /**
  * @author Jaineek Parikh
- * @author Johnny Test
  * @author Ananth Kuchibhotla
  * @author Rohan Upponi
  */
 
-public class MeepleMain extends AppCompatActivity {
+public class MeepleMain extends Activity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meeple_main);
+        switchScreen();
+    }
+
+    public void switchScreen() {
+        final Context context = this;
+        button = (Button) findViewById(R.id.switchButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, SettingsScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
