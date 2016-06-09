4f2aadf5a2d0164ee0bbe6b799b07e39dc59c29d
Jaineek Parikh <parikh20@purdue.edu>
2016-06-08 22:31:59 -0700

    Added Login Screen

---
+++ MyApplication/app/src/main/java/com/example/jaineek/meeplemain/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.jaineek.meeplemain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
