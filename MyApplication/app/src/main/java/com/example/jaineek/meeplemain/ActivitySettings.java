13d533be4830755e4f4301ab46337268d9be1451
Rupponi <rponi70@gmail.com>
2016-06-09 16:08:42 -0700

    Added Settings Menu

---
+++ MyApplication/app/src/main/java/com/example/jaineek/meeplemain/ActivitySettings.java
@@ -0,0 +1,13 @@
+package com.example.jaineek.meeplemain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitySettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_settings);
+    }
+}
