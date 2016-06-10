185aaa2860be7c6aadecc158ad6855eccaa532fc
Jaineek Parikh <parikh20@purdue.edu>
2016-06-09 17:40:50 -0700

    Made buttons and clickable text work

--- MyApplication/app/src/main/java/com/example/jaineek/meeplemain/LoginActivity.java
+++ MyApplication/app/src/main/java/com/example/jaineek/meeplemain/LoginActivity.java
@@ -1,15 +1,33 @@
 package com.example.jaineek.meeplemain;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
+
+    TextView to_register_activity;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        to_register_activity = (TextView) findViewById(R.id.dont_have_account_clickable);
+        to_register_activity.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(getApplicationContext(), MeepleMain.class);
+                startActivity(registerIntent);
+            }
+        });
     }
 
 
--- MyApplication/app/src/main/java/com/example/jaineek/meeplemain/MeepleMain.java
+++ MyApplication/app/src/main/java/com/example/jaineek/meeplemain/MeepleMain.java
@@ -1,6 +1,11 @@
 package com.example.jaineek.meeplemain;
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -9,11 +14,35 @@ import android.widget.Toast;
  * @author Rohan Upponi
  */
 
-public class MeepleMain extends AppCompatActivity {
+public class MeepleMain extends Activity implements View.OnClickListener{
+
+    TextView to_login_activity;
+    Button next_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meeple_main);
+
+        to_login_activity = (TextView)findViewById(R.id.to_login_text);
+        to_login_activity.setOnClickListener(this);
+
+        next_button= (Button)findViewById(R.id.next_button);
+        next_button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.to_login_text:
+                Intent toLoginActivity = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(toLoginActivity);
+                break;
+
+            case R.id.next_button:
+                Intent nextRegisterPage = new Intent(getApplicationContext(), MeepleMain2.class);
+                startActivity(nextRegisterPage);
+                break;
+        }
     }
 }
