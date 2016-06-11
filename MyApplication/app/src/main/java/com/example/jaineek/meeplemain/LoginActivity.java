fecaad7eb17489d163a2874ecebe0a6aa941eaaf
ananthkk1997 <ananthkk1997@gmail.com>
2016-06-11 16:47:28 -0700

    created landscape layouts, linked activities through buttons/clickables, extracted all screen views in activities

--- MyApplication/app/src/main/java/com/example/jaineek/meeplemain/LoginActivity.java
+++ MyApplication/app/src/main/java/com/example/jaineek/meeplemain/LoginActivity.java
@@ -1,15 +1,34 @@
 package com.example.jaineek.meeplemain;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView mCreateAccountClick;   // m prefix indicates a member object
+    private Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mCreateAccountClick = (TextView) findViewById(R.id.dont_have_account_clickable);
+
+        //setting onClickListener for create new account clickable
+        mCreateAccountClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //creating an intent to change to register screen 1
+                Intent changeToRegister1 = new Intent(v.getContext(), MeepleMain.class);
+                startActivity(changeToRegister1);
+            }
+        });
+
     }
 
 
--- MyApplication/app/src/main/java/com/example/jaineek/meeplemain/MeepleMain.java
+++ MyApplication/app/src/main/java/com/example/jaineek/meeplemain/MeepleMain.java
@@ -1,6 +1,11 @@
 package com.example.jaineek.meeplemain;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -11,9 +16,52 @@ import android.widget.Toast;
 
 public class MeepleMain extends AppCompatActivity {
 
+    private TextView mLoginClick;   // m prefix indicates a member object
+    private Button mNextButton;
+    private EditText mName;
+    private EditText mEmailAddress;
+    private EditText mConfirmEmailAddress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meeple_main);
+
+        mLoginClick = (TextView) findViewById(R.id.have_account_clickable);
+        //setting onClickListener for have account clickable
+        mLoginClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //creating an intent to change back to Login screen
+                Intent changeToLogin = new Intent(v.getContext(), LoginActivity.class);
+                startActivity(changeToLogin);
+            }
+        });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        //setting onClickListener for Next Button
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    // check that all fields were filled out
+                    assert !mName.getText().toString().equals("");
+                    assert !mEmailAddress.getText().toString().equals("");
+                    assert !mConfirmEmailAddress.getText().toString().equals("");
+                    //creating intent to proceed to Register screen 2
+                    Intent changeToRegister2 = new Intent(v.getContext(), MeepleMain2.class);
+                    startActivity(changeToRegister2);
+                } catch (Exception e) {
+                    //create a Toast to alert user of input problem
+                    Toast.makeText(MeepleMain.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        /* DECLARING ALL INPUT EDITTEXT FIELDS */
+
+        mName = (EditText) findViewById(R.id.editText_name);
+        mEmailAddress = (EditText) findViewById(R.id.editText_email);
+        mConfirmEmailAddress = (EditText) findViewById(R.id.editText_confirm_email);
     }
 }
