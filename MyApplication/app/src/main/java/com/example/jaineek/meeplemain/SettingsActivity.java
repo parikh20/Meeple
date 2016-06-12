a11bee8933b083137f069af56ef3556328cd656f
Jaineek Parikh <parikh20@purdue.edu>
2016-06-12 15:58:13 -0700

    Preferences and shit

---
+++ MyApplication/app/src/main/java/com/example/jaineek/meeplemain/SettingsActivity.java
@@ -0,0 +1,29 @@
+package com.example.jaineek.meeplemain;
+
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new
+                MyPreferenceFragment()).commit();
+
+
+    }
+
+    public static class MyPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+}
