b3fabfa86093d38ebb6fd786c74e1d63aa37a272
Jaineek Parikh <parikh20@purdue.edu>
2016-07-04 19:01:30 -0700

    Fixed landscape crashes

--- MyApplication/app/src/main/java/com/example/jaineek/meeplemain/LoginActivity.java
+++ MyApplication/app/src/main/java/com/example/jaineek/meeplemain/LoginActivity.java
@@ -14,6 +14,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.phenotype.Configuration;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -146,4 +147,6 @@ public class LoginActivity extends AppCompatActivity {
             }
         }
     }
+    
+
 }
