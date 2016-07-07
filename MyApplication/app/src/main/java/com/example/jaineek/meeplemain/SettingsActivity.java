91663c94818fd41d561e10efc68cf6582a7857db
Jaineek Parikh <parikh20@purdue.edu>
2016-07-06 23:27:45 -0700

    Can now change email, password, username. Merged as well

--- MyApplication/app/src/main/java/com/example/jaineek/meeplemain/SettingsActivity.java
+++ MyApplication/app/src/main/java/com/example/jaineek/meeplemain/SettingsActivity.java
@@ -1,21 +1,75 @@
 package com.example.jaineek.meeplemain;
 
-import android.graphics.Color;
+import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceCategory;
-import android.preference.PreferenceFragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.*;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class SettingsActivity extends PreferenceActivity {
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    EditTextPreference editTextEmail;
+    EditTextPreference editTextPassword;
+    EditTextPreference editTextUsername;
+    String email;
+    String password;
+    String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Using preferences.xml in xml resource folder to draw activity
         addPreferencesFromResource(R.xml.preferences);
+
+        //Firebase references
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        //On Change listener for changing email
+        editTextEmail = (EditTextPreference) findPreference("key_change_email");
+        editTextEmail.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                email = editTextEmail.getText();
+                mUser.updateEmail(email);
+                return false;
+            }
+        });
+
+        //On Change listener for changing password EditText
+        // TODO: Setup password authentication and pop up confirmation window
+        editTextPassword = (EditTextPreference) findPreference("key_change_password");
+        editTextPassword.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                password = editTextPassword.getText();
+                mUser.updatePassword(password);
+                return false;
+            }
+        });
+
+        // On change Listener for changing username EditText
+        editTextUsername = (EditTextPreference) findPreference("key_change_username");
+        editTextUsername.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                username = editTextUsername.getText();
+                UserProfileChangeRequest profileUpdates =
+                        new UserProfileChangeRequest.Builder().setDisplayName(username).build();
+
+                // Change profile
+                mUser.updateProfile(profileUpdates);
+                return false;
+            }
+        });
     }
+
+
+
 }
