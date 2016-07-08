552084c1e999a093ffe0b5a7fbb776f6c1d51a83
ananthkk1997 <ananthkk1997@gmail.com>
2016-07-07 17:40:50 -0700

    got tabs to look right.

--- MyApplication/app/src/main/java/com/example/jaineek/meeplemain/FeedActivity.java
+++ MyApplication/app/src/main/java/com/example/jaineek/meeplemain/FeedActivity.java
@@ -3,23 +3,23 @@ package com.example.jaineek.meeplemain;
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,7 +29,7 @@ public class FeedActivity extends AppCompatActivity {
     ViewPager mViewPager;
     ActionBar mActionBar;
     List<MeepleFragment> mFragmentList;
-    TabLayout mSlidingTabLayout;
+    TabLayout mTabLayout;
 
     final static String TAG = "FeedActivity";
 
@@ -40,13 +40,52 @@ public class FeedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
-        mViewPager = (ViewPager) findViewById(R.id.viewPager_activity_feed);
-
-
-        // Support fragments require getSupportFragmentManager
+        // Create Fragment pages, mFragmentList and add to FragmentManager
         FragmentManager fm = getSupportFragmentManager();
+        createAndAddFragments(fm);
 
-        // Create all Pages as Fragments
+        // Set up ViewPager
+        mViewPager = (ViewPager) findViewById(R.id.viewPager_activity_feed);
+        mPagerAdapter = new MeepleFragmentPagerAdapter(fm);
+        mViewPager.setAdapter(mPagerAdapter);
+
+        // Set up Tabs
+        setupTabsandTitles();
+
+        mActionBar = getSupportActionBar();
+    }
+
+    private void setupTabsandTitles() {
+        // Sets up Tabs with Custom Views
+        mTabLayout = (TabLayout) findViewById(R.id.sliding_tab_layout);
+        mTabLayout.setupWithViewPager(mViewPager);   // must be set after setting Adapter
+        final LayoutInflater localInflater = LayoutInflater.from(this);
+        mTabLayout.post(new Runnable() {
+            // Needed, otherwise tab titles wont work
+            @Override
+            public void run() {
+                mTabLayout.setupWithViewPager(mViewPager);
+
+                // Set custom View for each Tab
+                for (int i = 0; i < mTabLayout.getTabCount(); i++) {
+                    TabLayout.Tab tab = mTabLayout.getTabAt(i);
+                    RelativeLayout customTabLayout =  (RelativeLayout) localInflater
+                            .inflate(R.layout.custom_view_tab, mTabLayout, false);
+
+                    TextView tabTextView = (TextView) customTabLayout.findViewById(R.id.tab_title);
+                    tabTextView.setText(tab.getText());
+                    tab.setCustomView(customTabLayout);
+                    tab.select();
+                }
+            }
+        });
+
+
+    }
+
+    private void createAndAddFragments(FragmentManager fm) {
+        // Creates Fragment pages, mFragmentList and adds to FragmentManager
+
         LocalFeedFragment localFeedFragment = new LocalFeedFragment();
         MapFragment mapFragment = new MapFragment();
 
@@ -55,19 +94,12 @@ public class FeedActivity extends AppCompatActivity {
         mFragmentList.add(localFeedFragment);
         mFragmentList.add(mapFragment);
 
-
-        fm.beginTransaction().add(R.id.viewPager_activity_feed, localFeedFragment,
-                LocalFeedFragment.TAG_LOCAL_FEED).add(R.id.viewPager_activity_feed,
-                mapFragment, MapFragment.TAG_MAP);
-
-        mPagerAdapter = new MeepleFragmentPagerAdapter(fm);
-
-        mViewPager.setAdapter(mPagerAdapter);
-
-        // Set up Tabs
-        mActionBar = getSupportActionBar();
-        mSlidingTabLayout = (TabLayout) findViewById(R.id.sliding_tab_layout);
-        mSlidingTabLayout.setupWithViewPager(mViewPager);   // must be set after setting Adapter
+        // Add to FragmentManager
+        fm.beginTransaction()
+                .add(R.id.viewPager_activity_feed, localFeedFragment,
+                        LocalFeedFragment.TAG_LOCAL_FEED)
+                .add(R.id.viewPager_activity_feed,
+                        mapFragment, MapFragment.TAG_MAP);
     }
 
     private void setActionBarTitle(String newTitle) {
@@ -75,10 +107,12 @@ public class FeedActivity extends AppCompatActivity {
         mActionBar.setTitle(newTitle);
     }
 
-    private class MeepleFragmentPagerAdapter extends FragmentStatePagerAdapter {
+    private class MeepleFragmentPagerAdapter extends FragmentStatePagerAdapter
+            implements ViewPager.OnPageChangeListener{
 
         private FragmentManager fragmentManager;
 
+        //FragmentStatePagerAdapter methods
         public MeepleFragmentPagerAdapter(FragmentManager fm) {
             super(fm);
             fragmentManager = fm;
@@ -88,7 +122,6 @@ public class FeedActivity extends AppCompatActivity {
         public Fragment getItem(int i) {
             // Returns Fragment at position i in FragmentManager
             MeepleFragment currentFragment = mFragmentList.get(i);
-            setActionBarTitle(currentFragment.getTitle());
             return (Fragment) currentFragment;
         }
 
@@ -102,8 +135,25 @@ public class FeedActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             // Returns Fragment title at position
             MeepleFragment currentFragment = mFragmentList.get(position);
-            setActionBarTitle(currentFragment.getTitle());
             return currentFragment.getTitle();
+        }
+
+        //ViewPager.onPageChangeListener methods
+        @Override
+        public void onPageSelected(int position) {
+            // Sets ActionBar title when Fragment page is selected
+            MeepleFragment currentFragment = mFragmentList.get(position);
+            setActionBarTitle(currentFragment.getTitle());
+        }
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            // Left blank
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+            // Left blank
         }
     }
 
@@ -117,6 +167,7 @@ public class FeedActivity extends AppCompatActivity {
 
     public void logout() {
         FirebaseAuth.getInstance().signOut();
+        // TODO: make intent for LoginActivity
     }
 
     @Override
@@ -124,10 +175,12 @@ public class FeedActivity extends AppCompatActivity {
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.action_logout:
+                // Log the user out
                 logout();
                 Log.d(TAG, "logout");
                 return true;
             case R.id.settings:
+                // Go to Settings Activity
                 Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
                 Intent toSettingsActivity = new Intent(FeedActivity.this, SettingsActivity.class);
                 startActivity(toSettingsActivity);
