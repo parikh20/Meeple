b1e46245b83866bba6a386f7002e7f89e6ccd0cb
Ananth Krishna Kuchibhotla <krishnak97@berkeley.edu>
2016-10-08 15:57:22 -0700

    added circle to map

--- MyApplication/app/src/main/java/com/example/jaineek/meeplemain/fragments/MyMapFragment.java
+++ MyApplication/app/src/main/java/com/example/jaineek/meeplemain/fragments/MyMapFragment.java
@@ -3,6 +3,7 @@ package com.example.jaineek.meeplemain.fragments;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -21,9 +22,12 @@ import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
 import com.firebase.geofire.GeoQuery;
 import com.firebase.geofire.GeoQueryEventListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -173,6 +177,49 @@ public class MyMapFragment extends Fragment implements MeepleFragment,
 
     /* GOOGLE MAPS METHODS */
 
+//    @Override
+//    public void onMapReady(GoogleMap map) {
+//        // Once map is ready, perform setup
+//        mMap = map;
+//        // Enables MyLocation button on map
+//        if (ContextCompat.checkSelfPermission(getActivity(),
+//                android.Manifest.permission.ACCESS_FINE_LOCATION)
+//                == PackageManager.PERMISSION_GRANTED) {
+//            // Check if Lcoation services are enabled
+//            mMap.setMyLocationEnabled(true);
+//            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+//
+//            // Allow custom Info Windows
+//            mMap.setInfoWindowAdapter(new MeepleInfoWindowAdapter(mLayoutInflater, mMarkersToPosts));
+//
+//            // Allow Info Window clicks
+//            mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+//                @Override
+//                public void onInfoWindowClick(Marker marker) {
+//                    // Send this post to ViewPostActivity
+//                    Post post = mMarkersToPosts.get(marker);
+//                    Intent toViewPostActivity = new Intent(getActivity(), ViewPostActivity.class);
+//                    toViewPostActivity.putExtra(ViewPostActivity.KEY_EXTRA_POST, post);
+//                    startActivity(toViewPostActivity);
+//                }
+//            });
+//
+//            if (mLastLocation != null) {
+//                GeoLocation geoCenter = new GeoLocation(mLastLocation.getLatitude(),
+//                        mLastLocation.getLongitude());
+//                GeoFire geoFire = new GeoFire(mGeoFireReference);
+//                GeoQuery geoQuery = geoFire.queryAtLocation(geoCenter, queryRadius);
+//
+//                setupGeoQueryWithMarkers(geoQuery);
+//            }
+//
+//        } else {
+//             // Notify the user that ACCESS LOCATION permission is disabled
+//            Toast.makeText(getActivity(), getString(R.string.error_location_not_supported),
+//                    Toast.LENGTH_LONG).show();
+//        }
+//    }
+
     @Override
     public void onMapReady(GoogleMap map) {
         // Once map is ready, perform setup
@@ -201,19 +248,54 @@ public class MyMapFragment extends Fragment implements MeepleFragment,
             });
 
             if (mLastLocation != null) {
+                // Create circle around user location
+                double userLat = mLastLocation.getLatitude();
+                double userLon = mLastLocation.getLongitude();
+                LatLng center = new LatLng(userLat, userLon);
+                Circle localCircle = mMap.addCircle(new CircleOptions().center(center)
+                    .radius(kmRadiusToCircleRadius(queryRadius))
+                        // Color hex format: 2 bit % transparency + 6 bit color
+                        .fillColor(0x401BB3F5)
+                        .strokeColor(0x883369E8));
+
+                Toast.makeText(getActivity(), TAG + ": " + Double.toString(queryRadius),
+                        Toast.LENGTH_SHORT).show();
+
+
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(center,
+                        getZoomLevel(localCircle)));
+
+                // Setup GeoFire query
                 GeoLocation geoCenter = new GeoLocation(mLastLocation.getLatitude(),
                         mLastLocation.getLongitude());
                 GeoFire geoFire = new GeoFire(mGeoFireReference);
                 GeoQuery geoQuery = geoFire.queryAtLocation(geoCenter, queryRadius);
-
                 setupGeoQueryWithMarkers(geoQuery);
             }
 
         } else {
-             // Notify the user that ACCESS LOCATION permission is disabled
+            // Notify the user that ACCESS LOCATION permission is disabled
             Toast.makeText(getActivity(), getString(R.string.error_location_not_supported),
-                    Toast.LENGTH_LONG).show();
+                    Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private float getZoomLevel(Circle circle) {
+        // Returns proper zoom level for circle
+        float zoomLevel = 11;
+        if (circle != null) {
+            double radius = circle.getRadius() + circle.getRadius() / 2;
+            double scale = radius / 500;
+            zoomLevel = (float) (16 - Math.log(scale) / Math.log(2));
+        }
+
+        float padding = (float) 0.25;
+        return zoomLevel - padding;
+    }
+
+    private double kmRadiusToCircleRadius(double km) {
+        // Returns radius in meters for CircleOptions
+        return km * 1000;
     }
 
     /* LIFECYCLE METHODS  -
